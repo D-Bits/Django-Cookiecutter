dc1e221a50af8fa72cb2023c2890cd7fa447cc69
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2019-10-19 11:35:26 -0700

    Added post-gen hook

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,25 @@
+from subprocess import run
+
+# Initialize a git repo, add, and commit files
+def git():
+
+    git_init = run(['git', 'init'])
+    git_init.check_returncode()
+
+    git_add = run(['git', 'add', '-A'])
+    git_add.check_returncode()
+
+    git_commit = run(['git', 'commit', '-m', 'initial commit'])
+    git_commit.check_returncode()
+
+    # Ask the user if they want to do a 'git remote' configuration
+    git_remote_choice = input('Would you also like to do a git remote configuration(y/n)?: ')
+
+    if git_remote_choice == "y":
+        remote_url = input('Enter the URL of your remote repository: ')
+        git_remote = run(['git', 'remote', 'add', 'origin', remote_url])
+        git_remote.check_returncode()
+    elif git_remote_choice == "n":
+        pass
+    else:
+        input("Invalid value entered. Press enter to exit.")
